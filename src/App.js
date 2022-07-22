1971eaa47e03924c3a76d16e9339120cdbe65fdb
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-22 13:42:12 +0100

    setingup folders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Mission from './components/Mission';
+import About from './components/About';
+import WhatWeDo from './components/WhatWeDo';
+import OurWorks from './components/OurWorks';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <Header />
+      <Hero />
+      <Mission />
+      <About />
+      <WhatWeDo />
+      <OurWorks />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
