6d868c43419167dc0bb2a72768ab2598ffb9e8f3
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-24 22:31:16 +0100

    contact us

--- src/components/ContactUs/Form.jsx
+++ src/components/ContactUs/Form.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+const list = [
+  "Front-End Development",
+  "Backend Development",
+  "UI/UX Design",
+  "SAAS Development",
+];
+const Listchoice = () => {
+  return (
+    <div className="d-flex">
+      {list &&
+        list.map((item, key) => (
+          <div className="listedItem mr-auto" key={key}>
+            <h3>{item}</h3>
+          </div>
+        ))}
+    </div>
+  );
+};
 
 function Form() {
   return (
     <div className="form">
       <span className="headForm my-4">Choose what you’d like to do</span>
-
+      <Listchoice />
       <form>
         <div className="form-container">
           <div className="input">
@@ -21,7 +39,12 @@ function Form() {
           <div className="input">
             <label htmlFor="name">Tell Us What You’d Like to Do</label>
 
-            <textarea className="message" required id="" rows="3"></textarea>
+            <textarea
+              className="message border-0"
+              required
+              id=""
+              rows="3"
+            ></textarea>
           </div>
         </div>
 
