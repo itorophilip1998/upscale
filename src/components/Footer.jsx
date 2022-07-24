ffe0e25300cfb631af70c4e65f6f344f037a7e59
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-24 23:52:05 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,55 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='footer'>
-      
-
+    <div className="footer">
+      <div className=" row m-0 ">
+        <div className="col-md-3 br text-right p-5">
+          <a className="navbar-brand" href="/">
+            <img src="/images/logo.png" alt="" className="logo" /> Upscale
+            Advisory
+          </a>
+        </div>
+        <div className="col-md-3 br">
+          <span className="headText">Offices</span>
+          <ul>
+            <li>
+              <img src="" alt="" />
+              <a href="/">Nigeria</a>
+            </li>
+            <li>
+              <img src="" alt="" />
+              <a href="/">Dusseldorf</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-6">
+          <span className="headText">Follow Us</span>
+          <ul>
+            <li>
+              <a href="/">
+                <img src="" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <img src="" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <img src="" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <img src="" alt="" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
