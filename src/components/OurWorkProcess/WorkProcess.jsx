6f2b09da7b8ec08ed369a8afe2b1cfc5ec9c09e5
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-24 13:53:12 +0100

    our process

--- src/components/OurWorkProcess/WorkProcess.jsx
+++ src/components/OurWorkProcess/WorkProcess.jsx
@@ -3,11 +3,13 @@ import React from "react";
 function WorkProcess({ item }) {
   return (
     <div className="ourworkprocess-box">
-      {item.isTop && <img src={`/images/${item.img}`} alt="" />}
-
-      <h2 className="header">{item.head}</h2>
-      <p>{item.text}</p>
       {!item.isTop && <img src={`/images/${item.img}`} alt="" />}
+
+      <div className="content">
+        <h2 className="header">{item.head}</h2>
+        <p>{item.text}</p>
+      </div>
+      {item.isTop && <img src={`/images/${item.img}`} alt="" />}
     </div>
   );
 }
--- src/components/OurWorkProcess/index.jsx
+++ src/components/OurWorkProcess/index.jsx
@@ -3,7 +3,7 @@ import { ourprocess as list } from "../../utils/db";
 import WorkProcess from "./WorkProcess";
 function OurWorkProcess() {
   return (
-    <div className="ourwork ourworkprocess py-5 ">
+    <div className="ourwork ourworkprocess  container py-5 ">
       <h1 className="heading pt-3 ">OUR WORK PROCESS</h1>
       <p>
         Our Works are very diverse and we work with several industries while
