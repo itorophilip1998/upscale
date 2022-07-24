3f5595b52df8682c6b483949b3a6f1ca8e48a3bc
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-24 15:55:36 +0100

    contact us

--- src/components/ContactUs/Form.jsx
+++ src/components/ContactUs/Form.jsx
@@ -3,9 +3,21 @@ import React from "react";
 function Form() {
   return (
     <div className="form">
-          <span>Choose what you’d like to do</span>
-          
-          
+      <span>Choose what you’d like to do</span>
+
+      <form>
+        <input type="text" required className="name" placeholder="Name" />
+        <input type="email" required className="emal" placeholder="Email" />
+        <textarea
+          className="message"
+          required
+          id=""
+          placeholder="Tell Us What You’d Like to Do"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button>Send</button>
+      </form>
     </div>
   );
 }
