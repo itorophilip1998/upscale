729bf417198ae16a9b64678685d54d86141b6897
Itoro Philip <itorophilip1998@gmail.com>
2022-08-22 18:02:41 +0100

    dev

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,19 @@ import React from "react";
 function Hero() {
   const scrollDown = () => {
     // window.scrollTo(0, 400);
-     window.scrollTo({
-       top: 500,
-       behavior: "smooth",
-     });
+    window.scrollTo({
+      top: 500,
+      behavior: "smooth",
+    });
   };
   return (
     <div className="hero">
       <p>
-        <div className="mb-4 p-md-2 p-4" data-aos="fade-down" data-aos-delay="600">
+        <div
+          className="mb-4 p-md-2 p-5"
+          data-aos="fade-down"
+          data-aos-delay="600"
+        >
           Your professional agency for mobile and web based
           <strong> software development</strong>
         </div>
